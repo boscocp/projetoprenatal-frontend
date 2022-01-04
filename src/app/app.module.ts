2a3dc51fe819776f7a1709876ca0788f9167f6ce
Adalberto Pereira <adalbertobosco@gmail.com>
2022-01-04 18:55:25 -0300

    minor adjustment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './home/login/login.component';
-
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { RegisterComponent } from './home/register/register.component';
 import { PatientModule } from './patient/patient.module';
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
